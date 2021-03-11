624a51752d266403ab7d6b1f66d2abddc33d2b2d
Muhammed Salih Tatar <msalihtatar@outlook.com>
2021-03-11 18:45:40 +0300

    1-Result interface and result classes  have been added to the Core Layer. 2-Messages class has been added to the Business Layer. 3-Test codes in the ConsoleUI Layer have been updated.

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //iki constructor da çalışabilir
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+        }
+        //overload
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        //Consturctor dışında set edilemez çünkü get read only özelliği barındırır
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
---
+++ FormAPI/Form1.cs
@@ -0,0 +1,53 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.Columns.Add("Product Id", 100);
+            listView1.Columns.Add("Product Name", 250);
+            listView1.Columns.Add("Category Name", 250);
+            listView1.Columns.Add("Units in Stock", 150);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                string id = "", productName = "", categoryName = "", unitsInStock = "";
+                id = Convert.ToString(product.ProductId);
+                productName = product.ProductName;
+                categoryName = product.CategoryName;
+                unitsInStock = Convert.ToString(product.UnitsInStock);
+                string[] details = { id, productName, categoryName, unitsInStock };
+
+                ListViewItem viewItem = new ListViewItem(details);
+                listView1.Items.Add(viewItem);
+            }
+        }
+    }
+}
