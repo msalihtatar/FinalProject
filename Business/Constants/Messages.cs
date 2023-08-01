f7a682be46c578d94f2f95acd97153fae9c51544
muhammed.tatar@unipa.com.tr <muhammed.tatar@unipa.com.tr>
2023-08-01 18:21:05 +0300

    JWT entegrasyonu eklendi.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,7 +1,4 @@
-﻿using Entities.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Core.Entities.Concrete;
 
 namespace Business.Constants
 {
@@ -14,5 +11,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün bulunabilir.";
         public static string ProductNameAlreadyExists = "Aynı isimde birden fazla ürün olamaz.";
         public static string CategoryLimitExceed = "En fazla 15 kategori bulunabilir.";
+        public static string AuthorizationDenied = "İşlem engellendi. Yetkiniz bulunmamaktadır.";
+        internal static string PasswordError = "Kullanıcı adı veya şifre hatalı.";
+        internal static string UserNotFound = "Kullanıcı bulunamadı.";
+        internal static string SuccessfulLogin = "Giriş başarılı.";
+        internal static string UserAlreadyExists = "Kullanıcı sistemde mevcuttur.";
+        internal static string AccessTokenCreated = "Token oluşturuldu.";
+        internal static string UserRegistered = "Kullanıcı kaydedildi.";
     }
 }
